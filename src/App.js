b5863e5130f4b6718396332d076d6d26f0611861
mustafa <mltm0204@gmail.com>
2021-12-24 16:33:20 +0300

    fourth commit

--- src/App.js
+++ src/App.js
@@ -49,7 +49,9 @@ function App() {
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
-          {alert.show && <Alert {...alert} removeAlert={showAlert}/>}     
+          {alert.show && <Alert {...alert} removeAlert={showAlert}
+          list={list}
+          />}     
           <h3>grocery bud</h3>
           <div className="form-control">
               <input type="text"
